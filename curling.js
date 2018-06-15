543972b3a50c6de6ce8700a76b33e3c571685aca
Amlambda <e.amanda.norberg@gmail.com>
2018-06-15 15:45:50 +0200

    blah

--- curling.js
+++ curling.js
@@ -1,5 +1,6 @@
 jQuery(window).on('load', function() {
 	var $ = jQuery;
+	var ballList = [];
 
 	class vector {
 		constructor(xVal, yVal) {
@@ -48,9 +49,12 @@ jQuery(window).on('load', function() {
 		}
 	};
 
-	var ball1 = new CurlingBall($('.ball1'));
-	var ball2 = new CurlingBall($('.ball2'));
-
+	//var ball1 = new CurlingBall($('.ball1'));
+	var ball2 = $('.ball2');
+	var ball1 = $('.ball1');
+	ballList.push(new CurlingBall($('.ball1')));
+	ballList.push(new CurlingBall($('.ball2')));
+	console.log(ball1);
 
 
 	// function createVec(x,
@@ -68,66 +72,81 @@ jQuery(window).on('load', function() {
 
 	//$(window).load(function() {
 	//console.log(ball1.ball[0]);
-	ball1.ball.addEventListener('mousedown',
+	ball1[0].addEventListener('mousedown',
 		onBallMouseDown, false);
-	ball1.ball.addEventListener('mouseup',
+	ball1[0].addEventListener('mouseup',
 		onBallMouseUp, false);
-	ball1.ball.addEventListener('touchstart',
+	ball1[0].addEventListener('touchstart',
 		onBallMouseDown, false);
-	ball1.ball.addEventListener('touchend',
+	ball1[0].addEventListener('touchend',
 		onBallMouseUp, false);
 	//document.body.appendChild(ball1.ball);
 
 
-	// document.addEventListener('mousemove', function(e) {
-	// 	onMouseMove(e);
-	// }, false);
-	// document.addEventListener('touchmove', function(e) {
-	// 	onTouchMove(e);
-	// }, false);
+	document.addEventListener('mousemove',
+		onMouseMove, false);
+	document.addEventListener('touchmove',
+		onTouchMove, false);
 
 	//});
 
 	function onMouseMove(e) {
 
-		if (ballClicked == true) {
-			ballX = e.pageX - 25;
-			ballY = e.pageY - 25;
-			ball.style.left = ballX + "px";
-			ball.style.top = ballY + "px";
+		if (ballList[ind].clicked == true) {
+			ballList[ind].position.x = e.pageX - 50;
+			ballList[ind].position.y = e.pageY - 50;
+			ballList[ind].ball.style.left = ballList[ind].position.x + "px";
+			ballList[ind].ball.style.top = ballList[ind].position.y + "px";
 		}
 
 	};
 
 	function onTouchMove(e) {
 
-		if (ballClicked == true) {
-			ballX = e.targetTouches[0].pageX - 25;
-			ballY = e.targetTouches[0].pageY - 25;
-			ball.style.left = ballX + "px";
-			ball.style.top = ballY + "px";
+		if (ballList[ind].clicked == true) {
+			ballList[ind].position.x = e.targetTouches[0].pageX - 50;
+			ballList[ind].position.y = e.targetTouches[0].pageY - 50;
+			ballList[ind].ball.style.left = ballList[ind].position.x + "px";
+			ballList[ind].ball.style.top = ballList[ind].position.y + "px";
 		}
 
 	};
+	var ind = 0;
+	var startPosX;
+	var startPosY;
+	var startTime;
+	var stopTime;
+	var midX = $(window).width() / 2;
+	var midY = $(window).height() / 2;
+
 
 	function onBallMouseDown(e) {
-		console.log("ball clicked " + e);
-		this.clicked = true;
-		console.log(this);
+		ballList[ind].clicked = true;
+		startPosX = ballList[ind].position.x;
+		startPosY = ballList[ind].position.y;
+		startTime = performance.now();
 
 	};
 
 	function onBallMouseUp(e) {
-		console.log("ball un clicked " + this);
-		this.clicked = false;
+		//console.log("ball un clicked " + this);
+		ballList[ind].clicked = false;
+		stopTime = performance.now();
+		var dist = Math.sqrt((ballList[ind].position.x - startPosX) * (ballList[ind].position.x - startPosX) +
+			(ballList[ind].position.y - startPosY) * (ballList[ind].position.y - startPosY));
+		var vel = dist * 500 / ((stopTime - startTime));
 
+		var ang = Math.atan2((ballList[ind].position.x - startPosX - midX), (ballList[ind].position.y - startPosY - midY)) * 180 / Math.PI;
+
+		console.log(ang);
 		//When the ball is released, it will be thrown at an angle relative to it's original position when it is thrown..  
 		//For testing purposes, let's throw a the ball at 90 degrees with a speed of 60 pixels per second.
-		//throwBall(60, 50, this);
+		throwBall(ang, vel);
+
 
 	};
 
-	function throwBall(angle, speed, el) {
+	function throwBall(angle, speed) {
 
 		var animationComplete = false;
 
@@ -138,7 +157,6 @@ jQuery(window).on('load', function() {
 		//speed is in pixels per second, so divide by 60 to get pixels per frame
 		var vx = Math.cos(rads) * speed / 60;
 		var vy = Math.sin(rads) * speed / 60;
-
 		var interval = setInterval(function() {
 
 			//How do I Calculate the new X and Y position for the ball during each frame, based on the angle and speed which it was thrown?
@@ -150,11 +168,11 @@ jQuery(window).on('load', function() {
 			// ball.style.left = ballX + "px";
 			// ball.style.top = ballY + "px";
 
-			el.position.x(el.position.x + vx);
-			el.position.y(el.position.y + vy);
+			ballList[ind].position.x = ballList[ind].position.x + vx;
+			ballList[ind].position.y = ballList[ind].position.y - vy;
 
-			el.ball.style.left = el.position.x + "px";
-			el.ball.style.top = el.position.x + "px";
+			ballList[ind].ball.style.left = ballList[ind].position.x + "px";
+			ballList[ind].ball.style.top = ballList[ind].position.y + "px";
 
 			if (animationComplete == true) {
 				clearInterval(interval);
